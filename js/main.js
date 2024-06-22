2698002f43bb32b8baf013c56ebfec138694bda7
WahyuAndikaRahadi <wahyuandikarahadi19@gmail.com>
2024-06-22 13:18:50 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
 $(document).ready(function () {
-    $(".menu-links li a").click(function (e) {
-      $(".menu-links li.active").removeClass("active");
-      var $parent = $(this).parent();
-      $parent.addClass("active");
-    });
-    $(".hamburger-icon").click(function () {
-      $(".menu-links").toggleClass("left");
-    });
-    $(".hamburger-icon").click(function () {
-      $(this).toggleClass("ham-style");
-    });
-    const themeSwitch = document.querySelector("#checkbox");
-    themeSwitch.addEventListener("change", () => {
-      document.body.classList.toggle("dark-theme");
-    });
+  $(".menu-links li a").click(function (e) {
+    $(".menu-links li.active").removeClass("active");
+    var $parent = $(this).parent();
+    $parent.addClass("active");
   });
-  
+
+  $(".hamburger-icon, .close-sidebar-btn").click(function () {
+    $(".menu-links").toggleClass("left");
+    $(".hamburger-icon").toggleClass("ham-style");
+  });
+
+  // Menambahkan event listener untuk tombol tutup
+  $(".close-sidebar-btn").click(function () {
+    $(".menu-links").removeClass("left");
+  });
+
+  const themeSwitch = document.querySelector("#checkbox");
+  themeSwitch.addEventListener("change", () => {
+    document.body.classList.toggle("dark-theme");
+  });
+});
