6fbd70ae07eaf94059faddbdf1d4600d155f24b9
WahyuAndikaRahadi <wahyuandikarahadi19@gmail.com>
2024-06-22 12:59:59 +0700

    first upload

---
+++ js/pluginindosad.js
@@ -0,0 +1,131 @@
+const progress = document.getElementById("progress");
+const song = document.getElementById("song");
+const controlIcon = document.getElementById("controlIcon");
+const playPauseButton = document.querySelector(".play-pause-btn");
+const forwardButton = document.querySelector(".controls button.forward");
+const backwardButton = document.querySelector(".controls button.backward");
+const songName = document.querySelector(".music-player h1");
+const artistName = document.querySelector(".music-player p");
+const Button = document.querySelector(".btn");
+
+const songs = [
+  {
+    title: "ADA Band",
+    name: "Yang Terbaik Bagimu",
+    source:
+      "music/terbaikbagimu.mp3",
+  },
+  {
+    title: "Yovie And Nuno",
+    name: "Manusia Biasa",
+    source:
+      "music/biasa.mp3",
+  },
+  {
+    title: "Yovie And Nuno",
+    name: "Mengejar Mimpi",
+    source:
+      "music/mimpi.mp3",
+  },
+  {
+    title: "Seventeen",
+    name: "Jaga Selalu Hatimu",
+    source:
+      "music/hatimu.mp3",
+  },
+  {
+    title: "Samsons",
+    name: "Kenangan Terindah",
+    source:
+      "music/kenangan.mp3",
+  },
+
+];
+
+let currentSongIndex = 3;
+
+function updateSongInfo() {
+  songName.textContent = songs[currentSongIndex].title;
+  artistName.textContent = songs[currentSongIndex].name;
+  song.src = songs[currentSongIndex].source;
+
+  song.addEventListener("loadeddata", function () {});
+}
+
+song.addEventListener("timeupdate", function () {
+  if (!song.paused) {
+    progress.value = song.currentTime;
+  }
+});
+
+song.addEventListener("loadedmetadata", function () {
+  progress.max = song.duration;
+  progress.value = song.currentTime;
+});
+
+function pauseSong() {
+  song.pause();
+  controlIcon.classList.remove("fa-pause");
+  controlIcon.classList.add("fa-play");
+}
+
+function playSong() {
+  song.play();
+  controlIcon.classList.add("fa-pause");
+  controlIcon.classList.remove("fa-play");
+}
+
+function playPause() {
+  if (song.paused) {
+    playSong();
+  } else {
+    pauseSong();
+  }
+}
+
+playPauseButton.addEventListener("click", playPause);
+
+progress.addEventListener("input", function () {
+  song.currentTime = progress.value;
+});
+
+progress.addEventListener("change", function () {
+  playSong();
+});
+
+forwardButton.addEventListener("click", function () {
+  currentSongIndex = (currentSongIndex + 1) % songs.length;
+  updateSongInfo();
+  playPause();
+});
+
+backwardButton.addEventListener("click", function () {
+  currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
+  updateSongInfo();
+  playPause();
+});
+
+
+
+updateSongInfo();
+
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  centeredSlides: true,
+  initialSlide: 3,
+  slidesPerView: "auto",
+  allowTouchMove: false,
+  spaceBetween: 40,
+  coverflowEffect: {
+    rotate: 25,
+    stretch: 0,
+    depth: 50,
+    modifier: 1,
+    slideShadows: false,
+  },
+  navigation: {
+    nextEl: ".forward",
+    prevEl: ".backward",
+  },
+});
+
